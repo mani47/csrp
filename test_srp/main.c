e49e06751671eb690d7741464076ffa6064cc594
mani47 <mm.mani@gmail.com>
2014-02-08 00:37:14 +0200

    Initial Commit

---
+++ test_srp/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test_srp
+//
+//  Created by Mani on 08/02/2014.
+//  Copyright (c) 2014 rastinware. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
